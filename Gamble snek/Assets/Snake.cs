b7fff1e0883f2f160867afc76d9e84ddbba5312d
Nicolo Frasco <nfrasco@iu.edu>
2020-11-05 20:09:21 -0500

    Midterm Game

---
+++ Gamble snek/Assets/Snake.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Snake : MonoBehaviour
+{
+
+    List<Transform> tail = new List<Transform>();
+    Vector2 dir = Vector2.right;
+    bool ate = false;
+    bool isDead = false;
+    public GameObject tailPrefab;
+    int _score = 0;
+    bool _isGameOver = false;
+    [SerializeField] GameObject _scoreText;
+    [SerializeField] GameObject _gameOverText;
+    void Start()
+    {
+        InvokeRepeating("Move", 0.08f, 0.08f);
+    }
+
+	void Update () {
+		if (!isDead) {
+			if (Input.GetKey (KeyCode.RightArrow))
+				dir = Vector2.right;
+			else if (Input.GetKey (KeyCode.DownArrow))
+				dir = -Vector2.up;
+			else if (Input.GetKey (KeyCode.LeftArrow))
+				dir = -Vector2.right;
+			else if (Input.GetKey (KeyCode.UpArrow))
+				dir = Vector2.up;
+		} else {
+			if (Input.GetKey(KeyCode.R)){
+				tail.Clear();
+				transform.position = new Vector3(0, 0, 0);
+				isDead = false;
+			}
+		}
+	}
+
+	void Move() {
+		if (!isDead) 
+        {
+			Vector2 v = transform.position;
+			transform.Translate (dir);
+			if (ate) 
+            {
+				GameObject g = (GameObject)Instantiate (tailPrefab,v,Quaternion.identity);
+				tail.Insert (0, g.transform);
+				ate = false;
+
+			} 
+            else if (tail.Count > 0) 
+            {
+					tail.Last ().position = v;
+					tail.Insert (0, tail.Last ());
+					tail.RemoveAt (tail.Count - 1);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.name.StartsWith("Food")) 
+        {
+		    ate = true;
+            Destroy(coll.gameObject);
+            IncreaseScore(1);
+        } 
+            else 
+        { 	
+		    isDead = true;
+            InitiateGameOver();
+		}
+	}
+
+    public void IncreaseScore(int amount){
+        _score += amount;
+        _scoreText.GetComponent<Text>().text = "Score: " + _score;
+
+    }
+
+    public void InitiateGameOver() {
+        _isGameOver = true;
+        _gameOverText.SetActive(true);
+		//Destroy();
+
+		     
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+		//	InitiateNewGame();
+        //}
+    }
+
+	public void InitiateNewGame() {
+        SceneManager.LoadScene(0);
+		_isGameOver = false;
+		isDead = false;
+		_gameOverText.SetActive(false);
+
+	}
+
+
+}
---
+++ Gamble snek/Assets/SpawnFood.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System;
+
+public class SpawnFood : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    public GameObject foodPrefab2;
+    public GameObject foodPrefab3;
+    public GameObject foodPrefab4;
+    
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    public GameObject[] FoodPrefabs;
+    public float spawnRate = 2.0f;
+    private float nextSpawn = 0.0f;
+    
+    void Start()
+    {
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+    void Update()
+    {
+
+     }
+        
+    void Spawn(){
+
+        int x = (int)Random.Range(borderRight.position.x,borderLeft.position.y);
+        int y = (int)Random.Range(borderBottom.position.x,borderTop.position.y);
+        
+        if (Time.time > nextSpawn) {
+            nextSpawn = Time.time + spawnRate;
+            GameObject piClone = Instantiate(FoodPrefabs[Random.Range(0, FoodPrefabs.Length)], new Vector2(x,y),Quaternion.identity) as GameObject;
+        }
+    }
+}
